913e36969394253f7b5a6bf31675504f78bb9156
Andrii <andriy.dyachuk95@gmail.com>
2024-09-02 18:27:40 +0300

    Created project skeleton, moved contracts part

---
+++ contracts/token/HubNttToken.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.23;
+
+import { BaseNttToken } from "./BaseNttToken.sol";
+
+contract HubNttToken is BaseNttToken {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _owner,
+        address _minter
+    ) BaseNttToken(_name, _symbol, _owner) {
+        minter = _minter;
+        _mint(minter, MAX_SUPPLY);
+    }
+}
---
+++ contracts/token/SpokeNttToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.23;
+
+import { BaseNttToken } from "./BaseNttToken.sol";
+
+contract SpokeNttToken is BaseNttToken {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _owner,
+        address _minter
+    ) BaseNttToken(_name, _symbol, _owner) {
+        minter = _minter;
+    }
+}
